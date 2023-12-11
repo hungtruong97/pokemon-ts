4cad9bfcb25ab02a09310004785213f9cd2ea07f
hungtruong97 <hungtruong97@gmail.com>
2023-12-11 18:20:04 +0700

    add custom hook modify code in Pokemon.tsx and QuizQuestion.tsx with custom hook

--- src/organisms/Pokemon.tsx
+++ src/organisms/Pokemon.tsx
@@ -1,25 +1,8 @@
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { Button, Modal } from "antd";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useTeam } from "../context/Team";
-
-interface PokemonProps {
-  id: string;
-  name: string;
-  img: string;
-  cardImg: string;
-  type?: string;
-  weight?: string;
-  height?: string;
-  abilities: AbilityProps[];
-}
-
-interface AbilityProps {
-  ability: {
-    name: string;
-  };
-}
+import usePokemonData from "../hook/usePokemonData";
 
 const Pokemon: React.FC = () => {
   //for sucess modal and button
@@ -40,7 +23,6 @@ const Pokemon: React.FC = () => {
   };
 
   //for error modal
-
   const showErrorModal = () => {
     setIsErrorModalOpen(true);
     setOpen(false);
@@ -57,14 +39,11 @@ const Pokemon: React.FC = () => {
   //for the Pokemon component
   const pokemonId = useParams().id;
   const navigate = useNavigate();
-
-  const [pokemon, setPokemon] = useState<PokemonProps | null>(null);
-
-  useEffect(() => {
-    pokemonId && getPokemon(pokemonId);
-  }, [pokemonId]);
-
+  const { pokemon, loading, error } = usePokemonData(Number(pokemonId));
   const teamContext = useTeam();
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   if (!teamContext) {
     return <div>Error: team context not available</div>;
@@ -82,125 +61,122 @@ const Pokemon: React.FC = () => {
     }
   };
 
-  //Function to get pokemon if provided id
-
-  const getPokemon = async (id: string) => {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemonData: PokemonProps = {
-      name: response.data.name,
-      id: response.data.id,
-      type: response.data.types[0].type.name,
-      img: response.data.sprites.other[`official-artwork`].front_default,
-      cardImg: response.data.sprites.front_default,
-      weight: response.data.weight,
-      height: response.data.height,
-      abilities: response.data.abilities,
-    };
-    setPokemon(pokemonData);
-  };
-
   return (
     <div
       style={{ display: "flex", flexDirection: "column", textAlign: "center" }}
     >
-      <div className="pokemon">
-        <h2>Your Pokemon is {pokemon ? pokemon.name : ""}</h2>
-        <img
-          src={pokemon ? pokemon.img : ""}
-          alt={pokemon ? pokemon.name : ""}
-        />
-        <h3>Type: {pokemon ? pokemon.type : ""} </h3>
-        <h3>Weight: {pokemon ? pokemon.weight : ""}</h3>
-        <h3>Height: {pokemon ? pokemon.height : ""}</h3>
-        <h3>
-          Abilities:{" "}
-          {pokemon &&
-            pokemon.abilities.map((item, index) => (
-              <span key={index}>
-                {item.ability.name}
-                {index !== pokemon.abilities.length - 1 ? "," : ""}{" "}
-              </span>
-            ))}
-        </h3>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "20px",
-        }}
-      >
-        <Button
-          type="default"
-          onClick={() => {
-            navigate(`/evolution/${pokemonId}`);
-          }}
-        >
-          See its evolution
-        </Button>
-        <Button
-          type="primary"
-          onClick={() => {
-            showModal();
-            if (pokemon) {
-              handleAddPokemon(pokemon.id, pokemon.name, pokemon.cardImg);
-            }
-          }}
-        >
-          Add to my team
-        </Button>
-        <Modal
-          title="Basic Modal"
-          open={open}
-          onOk={handleOk}
-          onCancel={handleCancel}
-          footer={[
+      {pokemon && (
+        <div>
+          <div className="pokemon">
+            <h2>Your Pokemon is {pokemon ? pokemon.name : ""}</h2>
+            <img
+              src={pokemon ? pokemon.img : ""}
+              alt={pokemon ? pokemon.name : ""}
+            />
+            <h3>
+              Type:{" "}
+              {pokemon &&
+                pokemon.type.map((item, index) => (
+                  <span key={index}>
+                    {item.type.name}
+                    {index !== pokemon.type?.length - 1 ? "," : ""}
+                  </span>
+                ))}
+            </h3>
+            <h3>Weight: {pokemon ? pokemon.weight : ""}</h3>
+            <h3>Height: {pokemon ? pokemon.height : ""}</h3>
+            <h3>
+              Abilities:{" "}
+              {pokemon &&
+                pokemon.abilities.map((item, index) => (
+                  <span key={index}>
+                    {item.ability.name}
+                    {index !== pokemon.abilities.length - 1 ? "," : ""}{" "}
+                  </span>
+                ))}
+            </h3>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
             <Button
-              key="back"
-              onClick={handleCancel}
-              style={{ marginRight: 20 }}
+              type="default"
+              onClick={() => {
+                navigate(`/evolution/${pokemonId}`);
+              }}
             >
-              Return
-            </Button>,
-            <Link to="/team">
-              <Button type="primary" key="team">
-                Go to my team
-              </Button>
-            </Link>,
-          ]}
-        >
-          <p>
-            You have added{" "}
-            <span style={{ color: "coral" }}>{pokemon?.name}</span> to your team
-          </p>
-        </Modal>
-        <Modal
-          title="Sorry"
-          open={isErrorModalOpen}
-          onOk={handleErrorModalOk}
-          onCancel={handleErrorModalCancel}
-          footer={[
+              See its evolution
+            </Button>
             <Button
-              key="back"
-              onClick={handleErrorModalCancel}
-              style={{ marginRight: 20 }}
+              type="primary"
+              onClick={() => {
+                showModal();
+                if (pokemon) {
+                  handleAddPokemon(pokemon.id, pokemon.name, pokemon.cardImg);
+                }
+              }}
             >
-              Cancel
-            </Button>,
-            <Link to="/team">
-              <Button type="primary" key="team">
-                Yes
-              </Button>
-            </Link>,
-          ]}
-        >
-          <p>
-            Your team is full. Do you want to remove a member of your team to
-            add this pokemon?
-          </p>
-        </Modal>
-      </div>
+              Add to my team
+            </Button>
+            <Modal
+              title="Basic Modal"
+              open={open}
+              onOk={handleOk}
+              onCancel={handleCancel}
+              footer={[
+                <Button
+                  key="back"
+                  onClick={handleCancel}
+                  style={{ marginRight: 20 }}
+                >
+                  Return
+                </Button>,
+                <Link to="/team">
+                  <Button type="primary" key="team">
+                    Go to my team
+                  </Button>
+                </Link>,
+              ]}
+            >
+              <p>
+                You have added{" "}
+                <span style={{ color: "coral" }}>{pokemon?.name}</span> to your
+                team
+              </p>
+            </Modal>
+            <Modal
+              title="Sorry"
+              open={isErrorModalOpen}
+              onOk={handleErrorModalOk}
+              onCancel={handleErrorModalCancel}
+              footer={[
+                <Button
+                  key="back"
+                  onClick={handleErrorModalCancel}
+                  style={{ marginRight: 20 }}
+                >
+                  Cancel
+                </Button>,
+                <Link to="/team">
+                  <Button type="primary" key="team">
+                    Yes
+                  </Button>
+                </Link>,
+              ]}
+            >
+              <p>
+                Your team is full. Do you want to remove a member of your team
+                to add this pokemon?
+              </p>
+            </Modal>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
