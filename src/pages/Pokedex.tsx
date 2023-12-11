7619f6520accc19f207c61ce21e16275ed0db38e
hungtruong97 <hungtruong97@gmail.com>
2023-12-11 22:01:40 +0700

    initiate branch

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,19 +1,5 @@
-import { Flex, Button } from "antd";
-
 const Pokedex: React.FC = () => {
-  return (
-    <Flex
-      justify="center"
-      align="center"
-      style={{ height: "100vh", flexDirection: "column" }}
-    >
-      <h1>Welcome to Pokeworld</h1>
-      <h2>Click on the button below to generate a random Pokemon</h2>
-      <Button type="primary" size="large">
-        Generate Pokemon
-      </Button>
-    </Flex>
-  );
+  return <div>Pokedex</div>;
 };
 
 export default Pokedex;
